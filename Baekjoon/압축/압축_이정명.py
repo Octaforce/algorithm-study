cf6b3880380d90271f1da22b1f908dfbcdf817c5
leejm9704@naver.com <leejm9704@naver.com>
2025-04-20 13:58:39 +0900

    solved 압축 - 32ms 32412kb

---
+++ Baekjoon/압축/압축_이정명.py
@@ -0,0 +1,21 @@
+
+# import sys
+# input = sys.stdin.readline
+
+s = input()
+curS = '1'
+length = 0
+stack = []
+
+for i in s:
+    if i == '(':
+        stack.append([length-1, int(curS)])
+        length = 0
+    elif i == ')':
+        curLen, multipleCnt = stack.pop()
+        length = curLen + (length * multipleCnt)
+    else :
+        curS = i
+        length += 1
+
+print(length)
