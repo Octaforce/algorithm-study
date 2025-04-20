0e6eb77bea698dedfa2b8667f5d857e6cff66f94
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2025-04-20 21:22:13 +0900

    unsolved 램ㄹ프

---
+++ Baekjoon/램프/램프_박세건.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws IOException {
+        /*
+        * 해결 방법을 찾지 못했습니다.
+        * */
+    }
+
+
+}
